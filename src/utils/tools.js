6f79b084aace4ba1b95983ed4f2f678a273dca51
bigzyp <568820328@qq.com>
2020-01-16 16:48:35 +0800

    fix: ios日期兼容性

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,9 +1,17 @@
+/*
+ * @Author: zengyangping
+ * @Date: 2020-01-15 16:00:22
+ * @LastEditors: zengyangping
+ * @LastEditTime: 2020-01-16 16:44:15
+ * @Description: 
+ * @FilePath: /zhuzhutool/src/utils/tools.js
+ */
 // 计算天数
 export const computeDays = ({start, target, isRepeat}) => {
     const startTime = start ? new Date(start) : new Date();
     const curYear = startTime.getFullYear();
     let targetTime = new Date(target);
-    const endDate = `-${targetTime.getMonth()+1}-${targetTime.getDate()}`;
+    const endDate = `/${targetTime.getMonth()+1}/${targetTime.getDate()}`;
     let time = startTime.getTime() - targetTime.getTime();
     if(isRepeat){
         targetTime = new Date(`${curYear}${endDate}`);
